7b21c99bc82b9701c7ec3055ffaa4aa6e88e534e
ninomiyadaisuke <58416077+ninomiyadaisuke@users.noreply.github.com>
2020-10-30 16:37:54 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>こんにちは!</h1>
+      <p>お元気ですか</p>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
